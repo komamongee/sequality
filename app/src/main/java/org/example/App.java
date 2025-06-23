b9b4d0186449dd6699d3161a6b6cab39eff2e130
komabashiri <komamongee@git.com>
2025-06-23 20:35:33 +0900

    CaluculateTest3

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -4,11 +4,27 @@
 package org.example;
 
 public class App {
-    public String getGreeting() {
-        return "Hello World!";
-    }
+  public String getGreeting() {
+    return "Hello World!";
+  }
 
-    public static void main(String[] args) {
-        System.out.println(new App().getGreeting());
-    }
+  public static void main(String[] args) {
+    System.out.println(new App().getGreeting());
+    // 2と3の合計と平均を計算して表示
+    int sum2And3 = new Calculate().sum(2, 3);
+    double avg2And3 = new Calculate().average(2, 3);
+    System.out.println("Sum of 2 and 3 is " + sum2And3 + ". Average is " + avg2And3 + ".");
+    // 1から10までの合計と平均を計算して表示
+    int sum1To10 = new Calculate().sumRange(1, 10);
+    double avg1To10 = new Calculate().averageRange(1, 10);
+    System.out.println("Sum of 1 to 10 is " + sum1To10 + ". Average is " + avg1To10 + ".");
+
+    // 1から10までの奇数の合計を計算して表示
+    int oddSum = new Calculate().sumOdd(1, 10);
+    System.out.println("Sum of odd of 1 to 10 is " + oddSum + ".");
+
+    // 1から10までの偶数の合計を計算して表示
+    int evenSum = new Calculate().sumEven(1, 10);
+    System.out.println("Sum of even of 1 to 10 is " + evenSum + ".");
+  }
 }
